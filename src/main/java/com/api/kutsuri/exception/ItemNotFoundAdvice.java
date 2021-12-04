690080b38f22fd8e5057d40780571f16df679f38
amonrg <amontesrg@gmail.com>
2021-12-03 23:47:13 -0600

    Reimplement itemNotFoundHandler method

--- src/main/java/com/api/kutsuri/exception/ItemNotFoundAdvice.java
+++ src/main/java/com/api/kutsuri/exception/ItemNotFoundAdvice.java
@@ -4,19 +4,18 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class ItemNotFoundAdvice {
     @ResponseBody
     @ExceptionHandler(ItemNotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    String itemNotFoundHandler(ItemNotFoundException ex) {
-        final Map<String, String> jsonResponse = new LinkedHashMap<>();
-        jsonResponse.put("message", ex.getMessage());
-        return jsonResponse.toString();
+    public ResponseEntity<Map<String, Object>> itemNotFoundHandler(ItemNotFoundException ex) {
+        final Map<String, Object> errorInfo = new LinkedHashMap<>();
+        errorInfo.put("errorMessage", ex.getMessage());
+        return new ResponseEntity<Map<String, Object>>(errorInfo, HttpStatus.NOT_FOUND);
     }
 }
